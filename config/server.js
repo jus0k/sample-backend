ad630b090768a2b474a326b9986d323d5752fd71
jus0k <jutjd115@gmail.com>
2021-10-31 15:01:29 +0900

    feat: 샘플 업데이트

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4342ec602653cecfd450c2708d19f0ba'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '28300f66-142b-4dbb-b8bb-ed10a8735e83'
+};
